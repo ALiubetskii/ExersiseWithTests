a9653383ffe4d304ce3dce9cf9f06c46cdeb6bd9
ext.alyubetskiy <ext.alyubetskiy@tinkoff.ru>
2020-04-13 22:01:55 +0300

    неработающий тест

--- src/test/java/FoldersOperations.java
+++ src/test/java/FoldersOperations.java
@@ -1,29 +1,43 @@
 import io.restassured.RestAssured;
+import io.restassured.response.ValidatableResponse;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.notNullValue;
 
 public class FoldersOperations {
 
-    RestAssuredSpecification spec = new RestAssuredSpecification();
-//    @BeforeEach
-//    void configureApi() {
-//        RestAssured.baseURI = "https://cloud-api.yandex.net:443/";
-//        RestAssured.requestSpecification = given()
-//                .header("Content-Type", "application/json")
-//                .header("Authorization","AgAAAAAr4rfPAADLW8AAA6UeHUg6g4OvNNoQeHw");
-//    }
+    @BeforeEach
+    void configureApi() {
+        RestAssured.baseURI = "https://cloud-api.yandex.net:443/v1/disk/resources";
+        RestAssured.requestSpecification = given()
+                .header("Content-Type", "application/json")
+                .header("Authorization","AgAAAAAr4rfPAADLW8AAA6UeHUg6g4OvNNoQeHw");
+    }
 
     @Test
     void createDeleteFolder() {
 
+        String pathForDelete =
         given()
-                .spec(spec)
                 .when()
                 .put("path", "myTestDiskForAutomation")
                 .then()
                 .assertThat()
-                .statusCode(201);
+                .statusCode(201)
+                .body("href", notNullValue())
+                .extract()
+                .path("href");
+
+
+        given()
+                .when()
+                .queryParam("path", pathForDelete)
+                .delete()
+                .then()
+                .assertThat()
+                .statusCode(204);
     }
+
 }
